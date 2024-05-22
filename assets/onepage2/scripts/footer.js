04dfafef01914b9bb91d4991b11502b5907f5937
SpectreSpammer <rnavarra.business@gmail.com>
2024-05-22 12:50:45 +0800

    change footer,.js

--- assets/onepage2/scripts/footer.js
+++ assets/onepage2/scripts/footer.js
@@ -1,4 +1,3 @@
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -10,6 +9,11 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     event.preventDefault(); // Prevent form submission
 
     modal.style.display = "block"; // Display the modal
+
+    // Assuming the form submission is successful, redirect the user after a certain delay
+    setTimeout(function() {
+        window.location.href = "https://spectrespammer.github.io/RNPC/";
+    }, 5000); // Redirect after 5 seconds (adjust the delay as needed)
 });
 
 // When the user clicks on <span> (x), close the modal
@@ -23,4 +27,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 };
-
